df6a73a825ae07ed6270820abd34165576767249
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-06-02 14:32:59 +0300

    Completed Assignment

---
+++ app/migrations/versions/73a6a2e6a84f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 73a6a2e6a84f
+Revises: 
+Create Date: 2023-06-02 14:11:06.311874
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '73a6a2e6a84f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
